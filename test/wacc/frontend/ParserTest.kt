77d2830b46a6254be98e1b5b66640fb766aba06f
nasir <nm219@ic.ac.uk>
2021-01-29 21:26:06 +0000

    added a test to verify the correctness of tokens generated by the lexical analyser for a simple input.

---
+++ test/wacc/frontend/ParserTest.kt
@@ -0,0 +1,35 @@
+package wacc.frontend
+
+import antlr.WaccLexer
+import antlr.WaccParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class ParserTest {
+
+
+    @Test
+    fun lexicalAnalyserGivesCorrectTokens (){
+        val input = CharStreams.fromString("begin skip end ")
+        val lexer = WaccLexer(input)
+        assertThat(lexer.nextToken().text, `is`("begin"))
+        assertThat(lexer.nextToken().text, `is`("skip"))
+        assertThat(lexer.nextToken().text, `is`("end"))
+    }
+
+
+    @Test
+    fun `can parse simple expr`() {
+        val input = CharStreams.fromString("begin 1+2 end")
+        val lexer = WaccLexer(input)
+        val tokens = CommonTokenStream(lexer)
+        val parser = WaccParser(tokens)
+        val tree = parser.program()
+        assertThat(tree.getChild(0).getChild(0).text, `is`("begin"))
+        assertThat(tree.getChild(1).text, `is`("1"))
+        assertThat(tree.getChild(2).getChild(0).text, `is`("+"))
+    }
+}
