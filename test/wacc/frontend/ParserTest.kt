ba750eb09081ce2b36de0c969c356895e81425ac
nasir <nm219@ic.ac.uk>
2021-01-29 23:00:48 +0000

    fixed test that verifies the correct parsing of a simple input.

--- test/wacc/frontend/ParserTest.kt
+++ test/wacc/frontend/ParserTest.kt
@@ -2,8 +2,10 @@ package wacc.frontend
 
 import antlr.WaccLexer
 import antlr.WaccParser
+import org.antlr.v4.runtime.BufferedTokenStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.TokenStream
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
 import org.junit.Test
@@ -25,11 +27,15 @@ class ParserTest {
     fun `can parse simple expr`() {
         val input = CharStreams.fromString("begin 1+2 end")
         val lexer = WaccLexer(input)
-        val tokens = CommonTokenStream(lexer)
+
+
+        val tokens = BufferedTokenStream(lexer)
         val parser = WaccParser(tokens)
         val tree = parser.program()
-        assertThat(tree.getChild(0).getChild(0).text, `is`("begin"))
+        assertThat(tree.getChild(0).text, `is`("begin"))
         assertThat(tree.getChild(1).text, `is`("1"))
-        assertThat(tree.getChild(2).getChild(0).text, `is`("+"))
+        assertThat(tree.getChild(2).text, `is`("+"))
+        assertThat(tree.getChild(3).text, `is`("2"))
+        assertThat(tree.getChild(4).text, `is`("end"))
     }
 }
