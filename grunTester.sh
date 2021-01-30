ada422a96ef8d532d5a5407247adbeb7475556be
leo <leounoki22@gmail.com>
2021-01-30 12:24:55 +0900

    update grunTester to check for all errors

--- grunTester.sh
+++ grunTester.sh
@@ -1,11 +1,23 @@
-for file in `find ./src/test/valid -type f -name '*.wacc'`
+echo 'running grunTester'
+
+make
+rm -f result.out
+touch result.out
+
+for FILE in `find ./src/test/valid -type f -name '*.wacc'` 
 do
-    > result.out
-    ./grun antlr.Wacc program "$file" >> result.out
-    if grep -q "mismatched" result.out
+    ERROR=$(./grun antlr.Wacc program $FILE 2>&1 >/dev/null)
+    if [ -z $ERROR]
     then 
-        echo "Failed $file"
+        echo "Success $FILE"
     else
-        echo "Success $file"
+        echo "Failed $FILE"
+        echo $FILE >> result.out 
+        echo $ERROR "\n" >> result.out
     fi
 done
+
+[ -s result.out ] || echo "all test cases pass!" >> result.out
+
+
+
