d1222c5f751c24e4f8844d39a11fb8cf576f19cf
nasir <nm219@ic.ac.uk>
2021-02-03 12:40:03 +0000

    Added an enum class for base types. Added get base type function to the identifier class

--- src/wacc/frontend/Identifier.kt
+++ src/wacc/frontend/Identifier.kt
@@ -1,3 +1,39 @@
 package wacc.frontend
 
-class Identifier {}
+/* Types should store something (...) */
+enum class Type(){
+    INT(),
+    BOOL(),
+    CHAR(),
+    PAIR(),
+    ARRAY(),
+
+//    STRING(),
+//    ARGS(),
+//    FUNC(),
+//    STAT(),
+//    PROGRAM(-1),
+//    NONE();
+}
+
+
+open class Identifier(val type:Type) {
+
+    open fun getBaseType():Type{
+        return type
+    }
+}
+open class INT(){}
+open class BOOL(){}
+open class CHAR(){}
+open class PAIR(){}
+open class ARRAY(){}
+
+
+//literals
+open class STRING(){}
+//ARGS(),
+//FUNC(),
+//STAT(),
+//PROGRAM(-1),
+//NONE();
