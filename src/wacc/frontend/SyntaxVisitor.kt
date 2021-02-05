030265724b98d997be586c4163a1fb0e36af4c4b
leo <leounoki22@gmail.com>
2021-02-06 01:25:46 +0900

    fix ifstat syntax visitor

--- src/wacc/frontend/SyntaxVisitor.kt
+++ src/wacc/frontend/SyntaxVisitor.kt
@@ -1,45 +1,55 @@
 package wacc.frontend
-
 import antlr.WaccLexer
 import antlr.WaccParser
 import antlr.WaccParserBaseVisitor
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import java.io.File
+import java.lang.Exception
 import java.lang.System.exit
-import kotlin.system.exitProcess
+import java.io.PrintStream
 
 fun main(){
-//    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/function/mutualRecursionNoReturn.wacc").inputStream())
-//    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/function/functionJunkAfterReturn.wacc").inputStream())
-//    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/function/functionConditionalNoReturn.wacc").inputStream())
-//    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/function/functionReturnInLoop.wacc").inputStream())
-//    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/function/functionNoReturn.wacc").inputStream())
-    val input = CharStreams.fromStream(File("wacc_examples/invalid/syntaxErr/variables/bigIntAssignment.wacc").inputStream())
+    //overrides the printstream to list for stand error and throw exception
+    class CustomPrintStream(out: PrintStream?) : PrintStream(out) {
+        override fun print(s: String) {
+            throw Exception("Syntax failure!")
+        }
+    }
 
-//    val input = CharStreams.fromStream(File("wacc_examples/valid/function/nested_functions/functionConditionalReturn.wacc").inputStream())
-    val lexer = WaccLexer(input)
-    val tokens = CommonTokenStream(lexer)
-    val parser = WaccParser(tokens)
-    val tree = parser.program()
-    val visitor = SyntaxVisitor()
-    visitor.visit(tree)
+    System.setErr(CustomPrintStream(System.err));
+    val folder = File("wacc_examples/invalid/syntaxErr")
+    val list = actionOnFiles(folder) { file ->
+        val input = CharStreams.fromStream(file.inputStream())
+        val lexer = WaccLexer(input)
+        try{
+            val tokens = CommonTokenStream(lexer)
+            val parser = WaccParser(tokens)
+            val tree = parser.program()
+            val syntaxVisitor = SyntaxVisitor()
+            syntaxVisitor.visit(tree)
+            System.out.println("shouldn't reach here!" + file.path)
+        }catch(e:Exception){
+            println(file.path + " syntax failure as expected!")
+        }
+    }
 }
 class SyntaxVisitor : WaccParserBaseVisitor<Void>() {
-
     override fun visitFunc(ctx: WaccParser.FuncContext): Void? {
         //check if function ends with return or exit
         val lastStat: WaccParser.StatContext = getLastStat(ctx.stat())
         var functionEndsWithExitOrReturn = isExitOrReturn(lastStat)
+
+        //if the last statement is ifStat, check both branch ends with return/exit
         if (lastStat is WaccParser.IfStatContext){
-            val ifLastStat = getLastStat(lastStat.stat(0))
-            val elseLastStat = getLastStat(lastStat.stat(1))
-            functionEndsWithExitOrReturn = isExitOrReturn(ifLastStat)
-                    && isExitOrReturn(elseLastStat)
+            functionEndsWithExitOrReturn =
+                ifStatEndsWithExitOrReturn(lastStat)
         }
+
         if(!functionEndsWithExitOrReturn){
             syntaxError()
         }
+
         return null
     }
 
@@ -57,14 +67,7 @@ class SyntaxVisitor : WaccParserBaseVisitor<Void>() {
         exit(100)
     }
 
-    //checks if the given statement is EXIT or RETURN
-    private fun isExitOrReturn(statCtx: WaccParser.StatContext):Boolean {
-        if(statCtx is WaccParser.ActionStatContext &&
-            (statCtx.EXIT() != null || statCtx.RETURN() != null)){
-            return true
-        }
-        return false
-    }
+
 
     //recursively search for the last statement
     private fun getLastStat(stat: WaccParser.StatContext): WaccParser.StatContext {
@@ -79,5 +82,33 @@ class SyntaxVisitor : WaccParserBaseVisitor<Void>() {
         }
 
         return stat
+    }
+
+    //check if statement ends with return/exit
+    private fun ifStatEndsWithExitOrReturn(stat: WaccParser.IfStatContext) : Boolean {
+        //check if branch ends with return/exit
+        val ifLastStat = getLastStat(stat.stat(0))
+        var endsWithExitOrReturn = isExitOrReturn(ifLastStat)
+        if(!endsWithExitOrReturn && ifLastStat is WaccParser.IfStatContext){
+            endsWithExitOrReturn = ifStatEndsWithExitOrReturn(ifLastStat)
+        }
+
+        //check else branch ends with return/exit
+        val elseLastStat = getLastStat(stat.stat(1))
+        if(endsWithExitOrReturn
+            && elseLastStat is WaccParser.IfStatContext){
+            endsWithExitOrReturn = ifStatEndsWithExitOrReturn(elseLastStat)
+        }
+
+        return endsWithExitOrReturn
+    }
+
+    //checks if the given statement is EXIT or RETURN
+    private fun isExitOrReturn(statCtx: WaccParser.StatContext):Boolean {
+        if(statCtx is WaccParser.ActionStatContext &&
+            (statCtx.EXIT() != null || statCtx.RETURN() != null)){
+            return true
+        }
+        return false
     }
 }
