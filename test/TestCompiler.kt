a09278851bf77ca748ef48b16e327370441b6942
nasir <nm219@ic.ac.uk>
2021-02-06 18:22:30 +0000

    Added try catch blocks to the testing compiler.

--- test/TestCompiler.kt
+++ test/TestCompiler.kt
@@ -3,13 +3,15 @@ import antlr.WaccLexer
 import antlr.WaccParser
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import wacc.frontend.exceptions.SemanticException
+import wacc.frontend.exceptions.SyntaxException
 
 import java.lang.Exception
 
 class TestCompiler(private val fileName: String){
-    class CompilerResult(val exitCode: Int)
+    class CompilerResult(val exitCode: Int, val exception: Exception?)
 
-    fun testCompile(): CompilerResult {
+    fun testCompile(): CompilerResult? {
         val input = CharStreams.fromFileName(fileName)
         val lexer = WaccLexer(input)
         val tokens = CommonTokenStream(lexer)
@@ -18,21 +20,23 @@ class TestCompiler(private val fileName: String){
         var exception: Exception? = null
         val ast = try {
             //analysis
-        }catch (e: Exception) {
-//            Syntax errors
-//            System.err.println("Parse error in : $fileName")
-//            exitCode = 100
+        }catch (syntax: SyntaxException) {
+            System.err.println("Syntax Error in file: fileName")
+            exception = syntax
+            exitCode = 100
+            return null
+        }catch(semantic: SemanticException){
+            System.err.println("Semantic Error in file: fileName")
+            exception = semantic
+            exitCode = 200
+            return null
+        } catch(e:Exception) {
+            System.err.println("Error in file: fileName")
+            System.err.println("An exception was thrown that was not syntax or semantic")
+            exitCode = 1
         }
-//         catchsemantic error
-//            System.err.println("semantic error in: $fileName")
-//            exitCode = 200
-//        } catch anything else {
-//            System.err.println("Fatal error: $fileName")
-//            System.err.println("A non-syntactic, non-semantic exception was thrown!")
-//            exitCode = 1 //  error output
-
-        println("No Errors: $fileName")
-        return CompilerResult(0)
+        println("No Errors in file: fileName")
+        return CompilerResult(0, null)
     }
 
 
