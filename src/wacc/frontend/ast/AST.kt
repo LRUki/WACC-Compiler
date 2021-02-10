5fb5d73936210b0ca5a29c19c9f0ba2bd4c7d877
Krishan Sritharar <ks1619@ic.ac.uk>
2021-02-10 19:41:18 +0000

    Added SemanticTest and removed automatic gradle jar from makefile

--- src/wacc/frontend/ast/AST.kt
+++ src/wacc/frontend/ast/AST.kt
@@ -2,14 +2,20 @@ package wacc.frontend.ast
 
 import org.antlr.v4.runtime.ParserRuleContext
 import wacc.frontend.SymbolTable
+import wacc.frontend.ast.type.BaseType
+import wacc.frontend.ast.type.TypeAST
 
 interface AST {
     fun check(table: SymbolTable): Boolean {
         return true
     }
+    fun checkAndGetType(table: SymbolTable): TypeAST? {
+       return null
+    }
 }
 
 abstract class AbstractAST : AST {
     lateinit var ctx: ParserRuleContext
+    lateinit var confirmedType : BaseType
 }
 
