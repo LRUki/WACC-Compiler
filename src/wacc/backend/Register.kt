f0327ce14703e30cf3f95942b582036a9adbff2e
nasir <nm219@ic.ac.uk>
2021-02-17 23:29:08 +0000

    Added a condition and register enum to represent condition codes and registers

---
+++ src/wacc/backend/Register.kt
@@ -0,0 +1,14 @@
+package wacc.backend
+
+//16 visible registers
+enum class Register {
+
+    R0, R1, R2, R3,
+    R4, R5, R6, R7,
+    R8, R9, R10, R11, R12,
+
+    SP,   // R13 or Stack pointer
+    LR,   // R14 or Link register
+    PC,   // R15 Program counter
+    CPSR; // Current Program Status Register contains condition code flags, status bits, current mode bit
+}
