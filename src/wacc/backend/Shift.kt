5cbe1bf68c3abc1c9c09baca8a6c903b13cf797c
nasir <nm219@ic.ac.uk>
2021-02-18 10:22:30 +0000

    Added a shift enum to represent different types of shifting

---
+++ src/wacc/backend/Shift.kt
@@ -0,0 +1,10 @@
+package wacc.backend
+
+enum class Shift {
+
+    LSL, // Logical shift left
+    LSR, // Logical shift right
+    ASR, // Arithmetic shift right
+    ROR, // Rotate right
+    RRX  // Rotate right extended
+}
