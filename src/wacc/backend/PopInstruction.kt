5deac9b82b3ef35fda59eb0090fbdccafda35a95
nasir <nm219@ic.ac.uk>
2021-02-19 00:21:37 +0000

    Added push and pop instruction classes

---
+++ src/wacc/backend/PopInstruction.kt
@@ -0,0 +1,9 @@
+package wacc.backend
+
+class PopInstruction( private val registers: List<Register>) :Instruction{
+
+
+    //    Pop registers from stack POP <reglist>
+    //    Pop registers and PC from stack POP <reglist, PC>
+
+}
---
+++ src/wacc/backend/PushInstruction.kt
@@ -0,0 +1,7 @@
+package wacc.backend
+
+class PushInstruction( private val registers: List<Register>): Instruction  {
+
+//    Push registers onto stack PUSH <reglist>
+//    Push LR and registers onto stack PUSH <reglist, LR>
+}
