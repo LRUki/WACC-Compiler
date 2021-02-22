bb34fd58cd6c5155857f9d2ebc77c904a3f1272b
nasir <nm219@ic.ac.uk>
2021-02-22 12:06:24 +0000

    Added brief outline of structure for translate program function

--- src/wacc/backend/instruction/CodeGenerator.kt
+++ src/wacc/backend/instruction/CodeGenerator.kt
@@ -1,6 +1,32 @@
 package wacc.backend.instruction
 
+import com.sun.org.apache.bcel.internal.generic.PushInstruction
+import wacc.frontend.ast.program.ProgramAST
+
 class CodeGenerator {
+
+//    private fun translateProgram(program: ProgramAST): List<Instruction> {
+//        // Translate function definitions
+//        val functionInstructions = mutableListOf<Instruction>()
+//        for (function in program.funcList) {
+//            functionInstructions.addAll(translateFunction(function))
+//        }
+//
+//        val mainInstructions = mutableListOf<Instruction>()
+//        mainInstructions.add(FunctionLabel("main"))
+//        // AI: PUSH {lr}
+//        mainInstructions.add(PushInstr(Register.LR))
+//
+//        // Visit main program and add to instruction list
+//        mainInstructions.addAll(translateStatBlock(program.stats))
+//
+//        // AI: LDR r0, =0
+//        mainInstructions.add(LoadInstr("0", Register.R0))
+//        // AI: POP {pc}
+//        mainInstructions.add(PopInstr(Register.PC))
+//
+//
+//    }
 
 //    private fun translateStatement(stat: StatAST): List<Instruction> {
 //            SkipStatAST -> mutableListOf()
@@ -21,4 +47,6 @@ class CodeGenerator {
 
 //        }
 }
+
+
 
