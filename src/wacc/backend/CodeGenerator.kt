b0de27a76e4a2aa5d10a6cb0d4a8231ed27364c5
Henry Hu <haowei.hu19@imperial.ac.uk>
2021-02-23 16:33:47 +0800

    Finish backend CI and provide a dummy test for integration

---
+++ src/wacc/backend/CodeGenerator.kt
@@ -0,0 +1,8 @@
+package wacc.backend
+
+import wacc.frontend.ast.program.ProgramAST
+
+fun generateCode(ast : ProgramAST) : AssemblyCode {
+    // TODO: implement real code gen
+    return AssemblyCode(emptyList())
+}
