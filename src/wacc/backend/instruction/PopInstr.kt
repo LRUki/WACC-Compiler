eaed1fb6f172e2f49759a01245badb3dccd1dd07
Henry Hu <haowei.hu19@imperial.ac.uk>
2021-02-24 14:27:12 +0800

    Add utility functions to map registers to push or pop instructions

--- src/wacc/backend/instruction/PopInstr.kt
+++ src/wacc/backend/instruction/PopInstr.kt
@@ -2,11 +2,15 @@ package wacc.backend.instruction
 
 class PopInstr(val register: Register) : Instruction {
     override fun toAssembly(): String {
-        TODO("Not yet implemented")
+        return "POP {${register.toAssembly()}}"
     }
 
 
     //    Pop registers from stack POP <reglist>
     //    Pop registers and PC from stack POP <reglist, PC>
 
+}
+
+fun regsToPopInstrs(list: List<Register>) : List<PopInstr> {
+    return list.map { reg -> PopInstr(reg) }
 }
--- src/wacc/backend/instruction/PushInstr.kt
+++ src/wacc/backend/instruction/PushInstr.kt
@@ -2,9 +2,13 @@ package wacc.backend.instruction
 
 class PushInstr(val register: Register): Instruction {
     override fun toAssembly(): String {
-        TODO("Not yet implemented")
+        return "PUSH {${register.toAssembly()}}"
     }
 
 //    Push registers onto stack PUSH <reglist>
 //    Push LR and registers onto stack PUSH <reglist, LR>
+}
+
+fun regsToPushInstrs(list: List<Register>) : List<PushInstr> {
+    return list.map { reg -> PushInstr(reg) }
 }
