b290aede0ee019cbf720d8d0d9b47a5ff58a4f54
leo <leounoki22@gmail.com>
2021-02-24 18:12:50 +0900

    add data back to directiveInstr

--- src/wacc/backend/instruction/instrs/DirectiveInstr.kt
+++ src/wacc/backend/instruction/instrs/DirectiveInstr.kt
@@ -2,7 +2,7 @@ package wacc.backend.instruction.instrs
 
 import wacc.backend.instruction.Instruction
 
-class DirectiveInstr(val directive:String, val value:String=""): Instruction {
+data class DirectiveInstr(val directive:String, val value:String=""): Instruction {
     override fun toAssembly(): String {
         return ".$directive $value"
     }
