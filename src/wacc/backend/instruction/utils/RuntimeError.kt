f75789e8f745b3fcb2329cfbeee866a3026a7f22
nasir <nm219@ic.ac.uk>
2021-02-25 10:33:59 +0000

    Added arroyboundsCheck function to runtimeerror class which generates code to check array bounds at runtime

--- src/wacc/backend/instruction/utils/RuntimeError.kt
+++ src/wacc/backend/instruction/utils/RuntimeError.kt
@@ -28,8 +28,8 @@ class RuntimeError {
     enum class ErrorType(val msg: String) {
         NULL_REFERENCE("NullReferenceError: attempt to dereference a null reference\n" + 0.toChar()),
         DIVIDE_BY_ZERO("DivideByZeroError: divide or modulo by zero\n" + 0.toChar()),
-        ARRAY_INDEX_OUT_OF_BOUNDS_BIG("ArrayIndexOutOfBoundsError: index too large\n" + 0.toChar()),
-        ARRAY_INDEX_OUT_OF_BOUNDS_NEGATIVE("ArrayIndexOutOfBoundsError: negative index\n" + 0.toChar()),
+        LARGE_ARRAY_INDEX_OUT_OF_BOUNDS("ArrayIndexOutOfBoundsError: index too large\n" + 0.toChar()),
+        NEGATIVE_ARRAY_INDEX_OUT_OF_BOUNDS("ArrayIndexOutOfBoundsError: negative index\n" + 0.toChar()),
         OVERFLOW_ERROR("OverflowError: the result is too small/large to store in a 4-byte signed-integer.\n");
 
         override fun toString(): String {
@@ -94,6 +94,41 @@ class RuntimeError {
         //   BLEQ p_throw_runtime_error
         //   POP {pc}
     }
+
+    fun addArrayBoundsCheck(){
+        if (checkArrayBounds == null) {
+            val negativeMsgLabel = CodeGenerator.dataDirective.addStringLabel(ErrorType.NEGATIVE_ARRAY_INDEX_OUT_OF_BOUNDS.toString())
+            val tooLargeMsgLabel = CodeGenerator.dataDirective.addStringLabel(ErrorType.LARGE_ARRAY_INDEX_OUT_OF_BOUNDS.toString())
+
+            checkArrayBounds = listOf(
+                    checkArrayBoundsLabel,
+                    PushInstr(Register.LR),
+                    CompareInstr(Register.R0, null, 0),
+                    LoadInstr(Register.R0, null,  ImmediateLabel(negativeMsgLabel), Condition.LT),
+                    BranchInstr(Condition.LT, throwRuntimeErrorLabel, true),
+                    LoadInstr(Register.R1, null, RegisterAddr(Register.R1), Condition.AL),
+                    CompareInstr(Register.R0, Register.R1, null),
+                    LoadInstr(Register.R0, null,  ImmediateLabel(tooLargeMsgLabel), Condition.CS),
+                    BranchInstr(Condition.CS, throwRuntimeErrorLabel, true),
+                    PopInstr(Register.PC)
+            )
+            addThrowRuntimeError()
+            // p_check_array_bounds:
+            //   PUSH {lr}
+            //   CMP r0, #0
+            //   LDRLT r0, =msg_0
+            //   BLLT p_throw_runtime_error
+            //   LDR r1, [r1]
+            //   CMP r0, r1
+            //   LDRCS r0, =msg_1
+            //   BLCS p_throw_runtime_error
+            //   POP {pc}
+        }
+
+
+
+    }
+    //addthrowOverflowErrorLabel = Label("p_throw_overflow_error")
 
 
 }
