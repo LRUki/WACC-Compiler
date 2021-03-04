8b49bc69ab64e4a2eb66fd3fae909f888173f8ca
Henry Hu <haowei.hu19@imperial.ac.uk>
2021-03-05 00:25:15 +0800

    Make reused strings in CodePrinter constants

--- src/wacc/backend/CodePrinter.kt
+++ src/wacc/backend/CodePrinter.kt
@@ -2,12 +2,16 @@ package wacc.backend
 
 import wacc.backend.translate.instruction.Instruction
 
+const val DOT_DATA = ".data"
+const val DOT_TEXT = ".text"
+const val DOT_GLOBAL_MAIN = ".global main"
+
 fun printCode(instrs: List<Instruction>): String {
     val lines = instrs.map { instr -> instr.toAssembly() }
             .map { line -> if (shouldIndent(line)) "\t" + line else line }
     val builder = StringBuilder()
     lines.forEach {
-        val addEmptyLine = it.startsWith(".text") || it.startsWith(".data")
+        val addEmptyLine = it.startsWith(DOT_DATA) || it.startsWith(DOT_TEXT)
         if (addEmptyLine && builder.isNotBlank()) {
             builder.appendLine()
         }
@@ -23,9 +27,9 @@ fun shouldIndent(line: String): Boolean {
     return when {
         line.isBlank() ||
                 line[line.lastIndex] == ':' ||
-                line.startsWith(".text") ||
-                line.startsWith(".data") ||
-                line.startsWith(".global main") -> false
+                line.startsWith(DOT_DATA) ||
+                line.startsWith(DOT_TEXT) ||
+                line.startsWith(DOT_GLOBAL_MAIN) -> false
         else -> true
     }
 }
