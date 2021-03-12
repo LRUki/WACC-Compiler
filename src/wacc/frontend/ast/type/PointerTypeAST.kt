5e0b0e2cc1301b965ba6244a230ab6f93c0cb6b6
Henry Hu <haowei.hu19@imperial.ac.uk>
2021-03-12 17:58:52 +0800

    Make pointer nullable

--- src/wacc/frontend/ast/type/PointerTypeAST.kt
+++ src/wacc/frontend/ast/type/PointerTypeAST.kt
@@ -2,6 +2,9 @@ package wacc.frontend.ast.type
 
 class PointerTypeAST(val type: TypeAST) : TypeAST, Identifiable {
     override fun equals(other: Any?): Boolean {
+        if (other is AnyPairTypeAST) {
+            return true // So that can assign null to a pointer
+        }
         return other is PointerTypeAST && this.type == other.type
     }
 
